fe09d1b12f309cc2c494afcc091fd86bccb43589
Shawn <myking123321@gmail.com>
2024-11-29 02:07:28 +0700

    fix: validateLogin

--- middleware/validator/pasien.js
+++ middleware/validator/pasien.js
@@ -1,5 +1,7 @@
+import { BadRequestError } from "../../errors/BadRequestError.js";
 import { isValidDate } from "../../utils/dateValidator.js";
 import { isValidEmail } from "../../utils/emailValidator.js";
+import { isPassFormatCorrect, isPasswordInjection } from "../../utils/passwordValidator.js";
 
 export const validateRegister = async (req, res, next) => {
   const { nama, no_telp, email, jenis_kelamin, tanggal_lahir, id_kelurahan, password } = req.body;
@@ -15,6 +17,10 @@ export const validateRegister = async (req, res, next) => {
     throw new BadRequestError("jenis_kelamin must be either perempuan or laki");
   }
 
+  if(!isPassFormatCorrect(password)){
+    throw new BadRequestError('Password format doesn`t seem to be correct');
+  }
+
   if (!isValidDate(tanggal_lahir)) {
     throw new BadRequestError("tanggal_lahir format must be YYYY-MM-DD");
   }
@@ -25,3 +31,31 @@ export const validateRegister = async (req, res, next) => {
 
   next();
 };
+
+
+export const validateLogin = async (req,res,next) =>{
+  const { email, password} = req.body;
+
+  const requiredField = { email, password };
+
+  for (let field in requiredField) {
+    console.log(req.body[field] + field);
+    if (!req.body[field]) {
+      throw new BadRequestError(`${field} must be included`);
+    }
+  }
+
+  if(!isPassFormatCorrect(password)){
+    throw new BadRequestError('Password format doesn`t seem to be correct');
+  }
+  
+  if(!isPasswordInjection(password)){
+    throw new BadRequestError('Forbidden characters included');
+
+  }
+
+  next();
+
+
+
+}
