93210250aae54e5de13b22b2db23369f59f9766f
Shawn <myking123321@gmail.com>
2024-11-29 02:06:51 +0700

    fix:/login added validateLogin

--- routes/pasien.js
+++ routes/pasien.js
@@ -1,14 +1,13 @@
 import express from "express";
 import { registerPasien } from "../controllers/pasien.js";
 import { loginPasien } from "../controllers/pasien.js";
-import { isPasswordInjection } from "../utils/passwordValidator.js";
-import { validateRegister } from "../middleware/validator/pasien.js";
+import { validateLogin, validateRegister } from "../middleware/validator/pasien.js";
 
 const router = express.Router();
 
 router.post("/register", validateRegister, registerPasien);
 
-router.post("/login", loginPasien);
+router.post("/login", validateLogin, loginPasien);
 
 // router.post("/inject", isPasswordInjection );
 
