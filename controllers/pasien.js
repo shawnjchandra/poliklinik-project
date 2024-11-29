1adebf27f35c3eb757a07562a37fb60e9feac410
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-29 10:01:49 +0700

    feat:create validator for pasien login

--- controllers/pasien.js
+++ controllers/pasien.js
@@ -9,20 +9,7 @@ export const registerPasien = async (req, res) => {
 };
 
 export const loginPasien = async (req, res) => {
-  const { email, password } = req.body;
-
-  const requiredField = { email, password };
-
-  for (let field in requiredField) {
-    console.log(req.body[field] + field);
-    if (!req.body[field]) {
-      throw new BadRequestError(`${field} must be included`);
-    }
-  }
-
   const queryResult = await getPasien(req.body);
 
   return res.json(queryResult);
-
-  // console.log()
 };
--- middleware/validator/pasien.js
+++ middleware/validator/pasien.js
@@ -2,15 +2,14 @@ import { isValidDate } from "../../utils/dateValidator.js";
 import { isValidEmail } from "../../utils/emailValidator.js";
 
 export const validateRegister = async (req, res, next) => {
-  const { nama, no_telp, email, jenis_kelamin, tanggal_lahir, id_kelurahan, password } = req.body;
-
-  const requiredField = { nama, no_telp, email, jenis_kelamin, tanggal_lahir, id_kelurahan, password };
-  for (const field in requiredField) {
-    if (!requiredField[field]) {
+  const requiredField = ["nama", "no_telp", "email", "jenis_kelamin", "tanggal_lahir", "id_kelurahan", "password"];
+  for (const field of requiredField) {
+    if (!req.body[field]) {
       throw new BadRequestError(`field ${field} must be included`);
     }
   }
 
+  const { email, jenis_kelamin, tanggal_lahir } = req.body;
   if (jenis_kelamin !== "perempuan" && jenis_kelamin !== "laki") {
     throw new BadRequestError("jenis_kelamin must be either perempuan or laki");
   }
@@ -18,6 +17,23 @@ export const validateRegister = async (req, res, next) => {
   if (!isValidDate(tanggal_lahir)) {
     throw new BadRequestError("tanggal_lahir format must be YYYY-MM-DD");
   }
+
+  if (!isValidEmail(email)) {
+    throw new BadRequestError("invalid email format");
+  }
+
+  next();
+};
+
+export const validateLogin = async (req, res, next) => {
+  const requiredField = ["email", "password"];
+  for (const field of requiredField) {
+    if (!req.body[field]) {
+      throw new BadRequestError(`${field} must be included`);
+    }
+  }
+
+  const { email } = req.body;
 
   if (!isValidEmail(email)) {
     throw new BadRequestError("invalid email format");
