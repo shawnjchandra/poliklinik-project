692b704caab4977c79e8cb7766578c3378139aa7
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-12-06 00:28:57 +0700

    feat: register and login pegawai

---
+++ repository/pegawai.js
@@ -0,0 +1,41 @@
+import pool from "../db/db.js";
+
+export const insertPegawai = async ({
+  nama,
+  no_telp,
+  email,
+  password,
+  id_kelurahan,
+  NIP,
+  role,
+  biaya_kunjungan,
+  id_spesialisasi,
+}) => {
+  const insertPegawaiQuery =
+    "INSERT INTO Pegawai (nama, no_telp, email, password, id_kelurahan, NIP, role, biaya_kunjungan, id_spesialisasi) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)";
+
+  const values = [
+    nama,
+    no_telp,
+    email,
+    password,
+    id_kelurahan,
+    NIP,
+    role,
+    biaya_kunjungan,
+    id_spesialisasi,
+  ];
+
+  const queryResult = await pool.query(insertPegawaiQuery, values);
+
+  return queryResult;
+};
+
+export const getPegawaiByEmail = async (email) => {
+  const queryText = "SELECT * FROM Pegawai WHERE email=$1";
+  const values = [email];
+
+  const queryResult = await pool.query(queryText, values);
+
+  return queryResult;
+};
