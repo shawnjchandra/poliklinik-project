12fef87dadbd6d8cee019f226aefa7ce816355a4
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-12-06 10:12:46 +0700

    fix:remove detail dokter from get jadwal praktik repo

--- repository/jadwalPraktik.js
+++ repository/jadwalPraktik.js
@@ -11,8 +11,7 @@ export const insertJadwalPraktik = async ({ hari, start_time, end_time, kuota, i
 };
 
 export const getJadwalPraktik = async (id_pegawai) => {
-  const queryText =
-    "SELECT * FROM JadwalPraktikDokter jpd INNER JOIN Pegawai p ON jpd.id_pegawai = p.id_pegawai INNER JOIN Ruang r ON jpd.id_ruang = r.id_ruang INNER JOIN Spesialisasi s ON p.id_spesialisasi = s.id_spesialisasi WHERE p.id_pegawai = $1";
+  const queryText = "SELECT * FROM JadwalPraktikDokter jpd INNER JOIN Ruang r ON jpd.id_ruang = r.id_ruang WHERE jpd.id_pegawai = $1";
   const values = [id_pegawai];
 
   const queryResult = await pool.query(queryText, values);
