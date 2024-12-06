cdffc6641ae8b7e7d23803a9eb786afba27d4ca1
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-12-06 23:10:19 +0700

    fix:unresolved import

--- controllers/pendaftaran.js
+++ controllers/pendaftaran.js
@@ -1,28 +1,34 @@
 // import service
-import * as pendaftaranService from "../services/pendaftaran.js"
-
+import * as pendaftaranService from "../services/pendaftaran.js";
 
 // const addPendaftaran
-export const addPendaftaranOnline = async (req,res) => {
-    await pendaftaranService.addPendaftaranOnline(req.body);
+export const addPendaftaranOnline = async (req, res) => {
+  await pendaftaranService.addPendaftaranOnline(req.body);
 
-    return res.json({success: true});
-}
+  return res.json({ success: true });
+};
 
-export const addPendaftaranOffline = async (req,res) => {
-    await pendaftaranService.addPendaftaranOffline(req.body);
+export const addPendaftaranOffline = async (req, res) => {
+  await pendaftaranService.addPendaftaranOffline(req.body);
 
-    return res.json({success: true});
-}
+  return res.json({ success: true });
+};
 
-export const updateStatus = async (req,res) => {
-    await pendaftaranService.updateStatus(req.body);
+export const updateStatus = async (req, res) => {
+  await pendaftaranService.updateStatus(req.body);
 
-    return res.json({success: true});
-}
+  return res.json({ success: true });
+};
 
-export const updateAntrian = async (req,res) => {
-    await pendaftaranService.updateAntrian(req.body);
+export const updateAntrian = async (req, res) => {
+  await pendaftaranService.updateAntrian(req.body);
 
-    return res.json({success: true});
-}
+  return res.json({ success: true });
+};
+
+export const daftarUlang = async (req, res) => {
+  const { id_pendaftaran } = req.params;
+  await pendaftaranService.daftarUlang({ id_pendaftaran });
+
+  return res.json({ success: true });
+};
--- services/pendaftaran.js
+++ services/pendaftaran.js
@@ -1,36 +1,68 @@
+import pool from "../db/db.js";
+import { NotFoundError } from "../errors/NotFoundError.js";
 import * as pendaftaranRepo from "../repository/pendaftaran.js";
 import { formatDate } from "../utils/dateFormatter.js";
 
 // tanggal_daftar langsung dari program
-export const addPendaftaranOnline = async ({id_pasien}) => {
-    const tanggal_daftar = Date.now() + (1000*60*60*24);
-    
-    const formattedDate = formatDate(tanggal_daftar);
-    
-    const result = await pendaftaranRepo.addPendaftaran({status: 'pendaftaran', tanggal_daftar: formattedDate,id_pasien});
-    
-    return result;
+export const addPendaftaranOnline = async ({ id_pasien }) => {
+  const tanggal_daftar = Date.now() + 1000 * 60 * 60 * 24;
+
+  const formattedDate = formatDate(tanggal_daftar);
+
+  const result = await pendaftaranRepo.addPendaftaran({ status: "pendaftaran", tanggal_daftar: formattedDate, id_pasien });
+
+  return result;
 };
 
 // tanggal_daftar langsung dari program + 1 hari
-export const addPendaftaranOffline = async ({id_pasien}) => {
-    const tanggal_daftar = Date.now();
+export const addPendaftaranOffline = async ({ id_pasien }) => {
+  const tanggal_daftar = Date.now();
 
-    const formattedDate = formatDate(tanggal_daftar);
-    
-    const result = await pendaftaranRepo.addPendaftaran({status: 'pemanggilan', tanggal_daftar: formattedDate, id_pasien});
+  const formattedDate = formatDate(tanggal_daftar);
 
-    return result;
+  const result = await pendaftaranRepo.addPendaftaran({ status: "pemanggilan", tanggal_daftar: formattedDate, id_pasien });
+
+  return result;
 };
 
-export const updateStatus = async ({status, id_pendaftaran})=>{
-    const result = await pendaftaranRepo.updateStatus({status,id_pendaftaran});
+export const updateStatus = async ({ status, id_pendaftaran }) => {
+  const result = await pendaftaranRepo.updateStatus({ status, id_pendaftaran });
 
-    return result;
-}
+  return result;
+};
 
-export const updateAntrian = async ({antrian, id_pendaftaran})=>{
-    const result = await pendaftaranRepo.updateAntrian({antrian,id_pendaftaran});
+export const updateAntrian = async ({ id_pendaftaran }) => {
+  const result = await pendaftaranRepo.updateAntrian({ id_pendaftaran });
 
-    return result;
-}
+  return result;
+};
+
+export const daftarUlang = async ({ id_pendaftaran }) => {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const queryResultStatus = await pendaftaranRepo.updateStatus({ status: "pemanggilan", id_pendaftaran, prevStatus: "pendaftaran" }, client);
+
+    if (queryResultStatus.rowCount === 0) {
+      throw new NotFoundError(`id_pendaftaran ${id_pendaftaran} with status = 'pendaftaran' for today is not found`);
+    }
+
+    await client.query("SELECT pg_advisory_lock($1)", [12345]);
+    const queryResultLastAntrian = await pendaftaranRepo.getLatestAntrian(client);
+
+    const nextAntrian = queryResultLastAntrian.rows[0].latest_antrian + 1;
+
+    const queryResultAntrian = await pendaftaranRepo.updateAntrian({ id_pendaftaran, antrian: nextAntrian }, client);
+
+    await client.query("SELECT pg_advisory_unlock($1)", [12345]);
+    await client.query("COMMIT");
+  } catch (error) {
+    await client.query("ROLLBACK");
+
+    throw error;
+  } finally {
+    client.release();
+  }
+};
