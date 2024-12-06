5f78229a93563f5a386713337ef303838968dfe2
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-12-06 10:19:55 +0700

    fix:call pegawai service and adjust response

--- controllers/jadwalPraktik.js
+++ controllers/jadwalPraktik.js
@@ -1,4 +1,5 @@
 import * as jadwalPraktikService from "../services/jadwalPraktik.js";
+import * as pegawaiService from "../services/pegawai.js";
 
 export const createJadwalPraktik = async (req, res) => {
   const jadwalPraktik = await jadwalPraktikService.createJadwalPraktik(req.body);
@@ -9,7 +10,8 @@ export const getJadwalPraktik = async (req, res) => {
   const { id_pegawai } = req.params;
 
   const jadwalPraktik = await jadwalPraktikService.getJadwalPraktik(id_pegawai);
-  return res.json(jadwalPraktik);
+  const dokter = await pegawaiService.getDokterById(id_pegawai);
+  return res.json({ ...dokter, jadwal_praktik: jadwalPraktik });
 };
 
 export const updateJadwalPraktik = async (req, res) => {
