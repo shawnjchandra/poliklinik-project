496416e344c3dc669368ec4df41b581752440321
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-12-07 17:56:40 +0700

    fix:rekmedis repo and service

--- repository/rekmedis.js
+++ repository/rekmedis.js
@@ -1,71 +1,68 @@
 import pool from "../db/db.js";
 
-export const createRekamMedis = async (id_pasien) => {
-    const queryText = "INSERT INTO RekamMedis (id_pasien) VALUES ($1);";
+export const createRekamMedis = async ({ id_pasien, id_pendaftaran }) => {
+  const queryText = "INSERT INTO RekamMedis (id_pasien, id_pendaftaran) VALUES ($1, $2) RETURNING *";
 
-    const values = [id_pasien];
+  const values = [id_pasien, id_pendaftaran];
 
-    const queryResult = await pool.query(queryText,values);
+  const queryResult = await pool.query(queryText, values);
 
-    return queryResult;
+  return queryResult;
 };
 
 export const updateInformasiDasar = async ({ tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_rkm_med }) => {
-    console.log("di repo : "+ id_rkm_med);
-    
-    const queryText = "UPDATE RekamMedis SET tinggi_badan = $1, berat_badan = $2, golongan_darah = $3, diastolik= $4, sistolik = $5, denyut_nadi = $6 WHERE id_rkm_med = $7";
+  console.log("di repo : " + id_rkm_med);
 
-    const values = [tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_rkm_med];
+  const queryText = "UPDATE RekamMedis SET tinggi_badan = $1, berat_badan = $2, golongan_darah = $3, diastolik= $4, sistolik = $5, denyut_nadi = $6 WHERE id_rkm_med = $7";
 
-    const queryResult = await pool.query(queryText, values);
+  const values = [tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_rkm_med];
 
-    return queryResult;
-}
+  const queryResult = await pool.query(queryText, values);
 
-export const updateDiagnosaPasien = async ({resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med }) => {
+  return queryResult;
+};
 
-    const queryText = "UPDATE RekamMedis SET resep_obat = $1, prognosis_tindakan_lanjut = $2, diag_penunjang =$3, pemeriksaan_fisik = $4, pemeriksaan_penunjang = $5, riwayat_penyakit = $6, keluhan = $7 WHERE id_rkm_med = $8 ";
+export const updateDiagnosaPasien = async ({ resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med }) => {
+  const queryText = "UPDATE RekamMedis SET resep_obat = $1, prognosis_tindakan_lanjut = $2, diag_penunjang =$3, pemeriksaan_fisik = $4, pemeriksaan_penunjang = $5, riwayat_penyakit = $6, keluhan = $7 WHERE id_rkm_med = $8 ";
 
-    const values = [resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med];
+  const values = [resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med];
 
-    const queryResult = await pool.query(queryText, values);
+  const queryResult = await pool.query(queryText, values);
 
-    return queryResult;
+  return queryResult;
 };
 
 export const getRekamMedisByIdPasien = async (id_pasien) => {
-    const queryText = "SELECT * FROM RekamMedis WHERE id_pasien = $1";
+  const queryText = "SELECT * FROM RekamMedis WHERE id_pasien = $1";
 
-    const values = [id_pasien];
+  const values = [id_pasien];
 
-    const queryResult = await pool.query(queryText,values);
+  const queryResult = await pool.query(queryText, values);
 
-    return queryResult;
+  return queryResult;
 };
 
 export const getLatestRekamMedisByIdPasien = async (id_pasien) => {
-    const queryText = "SELECT * FROM RekamMedis WHERE id_pasien = $1 ORDER BY id_rkm_med DESC";
-    console.log("di latest "+id_pasien)
-    
-    const values = [id_pasien];
-    
-    const queryResult = await pool.query(queryText,values);
-    
-    // console.log("di latest isi row "+queryResult.rows[0].id_rkm_med);
-    return queryResult.rows[0] || null  ;
+  const queryText = "SELECT * FROM RekamMedis WHERE id_pasien = $1 ORDER BY id_rkm_med DESC";
+  console.log("di latest " + id_pasien);
+
+  const values = [id_pasien];
+
+  const queryResult = await pool.query(queryText, values);
+
+  // console.log("di latest isi row "+queryResult.rows[0].id_rkm_med);
+  return queryResult.rows[0] || null;
 };
 
 export const getRekamMedisByIdRM = async (id_rkm_med) => {
-    const queryText = "SELECT * FROM RekamMedis WHERE id_rkm_med = $1";
+  const queryText = "SELECT * FROM RekamMedis WHERE id_rkm_med = $1";
 
-    const values = [id_rkm_med];
+  const values = [id_rkm_med];
 
-    const queryResult = await pool.query(queryText,values);
+  const queryResult = await pool.query(queryText, values);
 
-    return queryResult;
+  return queryResult;
 };
-
-
 
 // CREATE TABLE RekamMedis(
 //     id_rkm_med INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
--- services/rekmedis.js
+++ services/rekmedis.js
@@ -1,80 +1,67 @@
-
 import { BadRequestError } from "../errors/BadRequestError.js";
 import * as rekMedRepo from "../repository/rekmedis.js";
 
 // Perawat selalu rekam medis yang baru , dan hanya edit informasi dasar pasien yang itu saja
 
 // Dokter bisa lihat semua rekam medis yang pernah ada, tapi hanya bisa tambah diagnosa pasien untuk yang saat itu saja
-export const createRekamMedis = async ({id_pasien}) => {
+export const createRekamMedis = async ({ id_pasien, id_pendaftaran }) => {
+  const queryResult = await rekMedRepo.createRekamMedis({ id_pasien, id_pendaftaran });
 
-    await rekMedRepo.createRekamMedis(id_pasien);
-    
-    const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
+  //   const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
 
-    return queryResultRekamMedis?.id_rkm_med || null;
-
-    
+  return queryResult.rows[0];
 };
 
+export const updateInformasiDasar = async ({ tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_pasien }) => {
+  const available = await checkAvailabilityRKM(id_pasien);
 
-export const updateInformasiDasar = async ({tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_pasien}) => {
-    
-    const available = await checkAvailabilityRKM(id_pasien);
+  if (!available) {
+    throw new BadRequestError("There is no 'rekam medis' available for this user ");
+  }
 
-    if(!available){
-        throw new BadRequestError("There is no 'rekam medis' available for this user ");
-    }
+  const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
 
-    const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
+  const id = queryResultRekamMedis?.id_rkm_med || null;
 
-    const id = queryResultRekamMedis?.id_rkm_med || null;
+  if (id === null) {
+    throw new BadRequestError("There are no rows found");
+  }
 
+  await rekMedRepo.updateInformasiDasar({ tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_rkm_med: id });
 
-    if(id === null){
-        throw new BadRequestError("There are no rows found");
-    }
-
-    await rekMedRepo.updateInformasiDasar({tinggi_badan, berat_badan, golongan_darah, diastolik, sistolik, denyut_nadi, id_rkm_med: id});
-
-    return ({success : true});
+  return { success: true };
 };
 
-export const updateDiagnosaPasien = async ({resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_pasien}) => {
-    
-    const available = await checkAvailabilityRKM(id_pasien);
+export const updateDiagnosaPasien = async ({ resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_pasien }) => {
+  const available = await checkAvailabilityRKM(id_pasien);
 
-    if(!available){
-        throw new BadRequestError("There is no 'rekam medis' available for this user ");
-    }
+  if (!available) {
+    throw new BadRequestError("There is no 'rekam medis' available for this user ");
+  }
 
-    const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
+  const queryResultRekamMedis = await rekMedRepo.getLatestRekamMedisByIdPasien(id_pasien);
 
-    const id = queryResultRekamMedis?.id_rkm_med || null;
+  const id = queryResultRekamMedis?.id_rkm_med || null;
 
+  if (id === null) {
+    throw new BadRequestError("There are no rows found");
+  }
 
-    if(id === null){
-        throw new BadRequestError("There are no rows found");
-    }
+  await rekMedRepo.updateDiagnosaPasien({ resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med: id });
 
-    await rekMedRepo.updateDiagnosaPasien({resep_obat, prognosis, diag_penunjang, pemeriksaan_fisik, pemeriksaan_penunjang, riwayat_penyakit, keluhan, id_rkm_med : id});
-    
-    return ({success : true});
-
+  return { success: true };
 };
 
-export const getRekamMedis = async ({id_rkm_med}) => {
-    const queryResult = await rekMedRepo.getRekamMedisByIdRM(id_rkm_med);
-
+export const getRekamMedis = async ({ id_rkm_med }) => {
+  const queryResult = await rekMedRepo.getRekamMedisByIdRM(id_rkm_med);
 };
 
 const checkAvailabilityRKM = async (id_pasien) => {
-    console.log("check availability : "+id_pasien);
-    
-    const rekamMedisQueryResult = await rekMedRepo.getRekamMedisByIdPasien(id_pasien);
+  console.log("check availability : " + id_pasien);
 
-    console.log("rowCount : "+ rekamMedisQueryResult.rowCount);
+  const rekamMedisQueryResult = await rekMedRepo.getRekamMedisByIdPasien(id_pasien);
 
-    return rekamMedisQueryResult.rowCount > 0 ? true : false; 
+  console.log("rowCount : " + rekamMedisQueryResult.rowCount);
+
+  return rekamMedisQueryResult.rowCount > 0 ? true : false;
 };
-
-
