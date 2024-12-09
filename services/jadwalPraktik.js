ba721e7bd46efedda505fc181f7596d352e296d2
bertrandOrlando <91016925+Frox8@users.noreply.github.com>
2024-12-09 22:18:13 +0700

    feat: add get all jadwal dokter

--- services/jadwalPraktik.js
+++ services/jadwalPraktik.js
@@ -1,13 +1,52 @@
 import { NotFoundError } from "../errors/NotFoundError.js";
 import * as jadwalPraktikRepo from "../repository/jadwalPraktik.js";
+import * as dokterRepo from "../repository/pegawai.js";
 
-export const createJadwalPraktik = async ({ hari, start_time, end_time, kuota, id_pegawai, id_ruang }) => {
-  
-  const queryResult = await jadwalPraktikRepo.insertJadwalPraktik({ hari, start_time, end_time, kuota, id_pegawai, id_ruang });
+export const createJadwalPraktik = async ({
+  hari,
+  start_time,
+  end_time,
+  kuota,
+  id_pegawai,
+  id_ruang,
+}) => {
+  const queryResult = await jadwalPraktikRepo.insertJadwalPraktik({
+    hari,
+    start_time,
+    end_time,
+    kuota,
+    id_pegawai,
+    id_ruang,
+  });
 
   const idJadwalPraktik = queryResult.rows[0].id_jadwal;
 
-  return { hari, start_time, end_time, kuota, id_pegawai, id_ruang, id_jadwal: idJadwalPraktik };
+  return {
+    hari,
+    start_time,
+    end_time,
+    kuota,
+    id_pegawai,
+    id_ruang,
+    id_jadwal: idJadwalPraktik,
+  };
+};
+
+export const getAllJadwalPraktik = async () => {
+  const jadwalPraktikResult = await jadwalPraktikRepo.getAllJadwalPraktik();
+
+  const dokterResult = await dokterRepo.getAllDokter();
+
+  const result = Promise.all(
+    dokterResult.rows.map((dokter) => {
+      const jadwal = jadwalPraktikResult.rows.filter(
+        (row) => row.id_pegawai === dokter.id_pegawai
+      );
+      return { ...dokter, jadwal };
+    })
+  );
+
+  return result;
 };
 
 export const getJadwalPraktik = async (id_pegawai) => {
